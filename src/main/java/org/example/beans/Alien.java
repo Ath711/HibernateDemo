a4a38f3061d8c712d999db724f110d2e8dbda7bd
AK <atharvakulkarni711@gmail.com>
2024-01-18 15:32:21 +0530

    added more information about @Entity @Column etc annotation

--- src/main/java/org/example/beans/Alien.java
+++ src/main/java/org/example/beans/Alien.java
@@ -1,17 +1,35 @@
 package org.example.beans;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 
-// each class corresponds to one table in DB
-// and each object corresponds to one row in table
-@Entity  // specifies table in DB with which this entity or class is mapped
+/*
+ each class corresponds to one table in DB
+ and each object corresponds to one row in table
+
+ @Entity   specifies table in DB with which this entity or class is mapped
+
+ @Entity(name = "NewAlien")  if we don't want class name to be table name which is default, we can provide one
+
+ the above annotation will also set Entity name as NewAlien, Basically there are 3 things
+ class name, entity name and table name
+ table name is derived from entity name
+*/
+
+// if we don't want to change the entity name
+@Entity
+@Table(name = "NewAlien")
 public class Alien {
 
     @Id  // alienId will be considered as primary key now
     private int alienId;
+
+    @Transient // aName will not be stored in DB
     private String aName;
+
+
+    @Column(name = "alienColor")
+    // we can also set column names in table, if we don't want one with default that is variable names
     private String color;
 
     public int getAlienId() {
